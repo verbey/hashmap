f3744be56313128724795987ca59af11187ac13d
Verbiturum <user@example.com>
2024-02-08 17:40:34 +0200

    Add constructor method to HashMap

--- hashmap.js
+++ hashmap.js
@@ -1,1 +1,20 @@
 const LinkedList = require("./linkedlist.js");
+
+class HashMap {
+	constructor() {
+		this.buckets = [];
+		for (let i = 0; i < 16; i++) {
+			this.buckets[i] = new LinkedList();
+		}
+	}
+	hash(key) {
+		let hashCode = 0;
+
+		const primeNumber = 31;
+		for (let i = 0; i < key.length; i++) {
+			hashCode = primeNumber * hashCode + key.charCodeAt(i);
+		}
+
+		return hashCode;
+	}
+};
