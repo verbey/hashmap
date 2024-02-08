e642fed61bbae55dd5f49fbcace4c67062406ea6
Verbiturum <user@example.com>
2024-02-08 17:51:29 +0200

    Add set method

--- hashmap.js
+++ hashmap.js
@@ -3,7 +3,7 @@ const LinkedList = require("./linkedlist.js");
 class HashMap {
 	constructor() {
 		this.buckets = [];
-		for (let i = 0; i < 16; i++) {
+		for (let i = 0; i < 31; i++) {
 			this.buckets[i] = new LinkedList();
 		}
 	}
@@ -16,5 +16,10 @@ class HashMap {
 		}
 
 		return hashCode;
+	}
+
+	set(key, value) {
+		const index = this.hash(key);
+		this.buckets[index].push(value);
 	}
 };
